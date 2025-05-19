3dd3ad7f9946118047904ea88873580e58299a35
emmydapson <iloriemmanuel00@gmail.com>
2025-05-19 16:16:46 +0100

    user route error fixed

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,8 +4,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { MatchModule } from '../match/match.module'; // ✅ import MatchModule
+import { UserController } from './user.controller';
 
 @Module({
+  controllers: [UserController],
   imports: [PrismaModule, MatchModule], // ✅ import MatchModule
   providers: [UserService], // ✅ remove MatchService
   exports: [UserService],
