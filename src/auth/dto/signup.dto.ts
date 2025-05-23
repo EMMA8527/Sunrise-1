00fa4996f8bb6d43e9d2193777d62c88a2791343
emmydapson <iloriemmanuel00@gmail.com>
2025-05-23 11:46:34 +0100

    gender case sensitive fixed

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,7 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class SignupDto {
+  @Transform(({ value }) => value?.toLowerCase())
   @IsEmail()
   email: string;
 
--- src/user/dto/set-gender.dto.ts
+++ src/user/dto/set-gender.dto.ts
@@ -1,7 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Transform } from 'class-transformer';
 import { IsEnum } from 'class-validator';
 import { Gender } from '@prisma/client';
 
 export class SetGenderDto {
-  @IsEnum(Gender)
+  @Transform(({ value }) => value?.toUpperCase())
+  @IsEnum(Gender, { message: 'Invalid gender. Use MALE, FEMALE, or OTHER.' })
   gender: Gender;
 }
