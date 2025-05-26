9994e6c5d715768c91fe95f0c11885c30c71dc1d
emmydapson <iloriemmanuel00@gmail.com>
2025-05-26 14:24:29 +0100

    cors orrigin permission for frontend website

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,13 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+  origin: ['http://localhost:3000', 'https://www.sunrise.cam'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+});
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
